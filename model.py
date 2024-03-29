13f26dfcc50c7ce083976503f5eab72dbd9a2856
M23CSA020RishabhJohri <m23csa020@iitj.ac.in>
2024-03-29 06:35:52 +0530

    Experiment 1: Changed kernel size

--- model.py
+++ model.py
@@ -35,6 +35,21 @@ class CNN(nn.Module):
         x = x.view(-1, 7*7*64)
         x = F.relu(self.fc1(x))
         return F.log_softmax(self.fc2(x), dim=1)
+class CNN_Modified_Kernel(nn.Module):
+    def __init__(self):
+        super(CNN_Modified_Kernel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)  # Changed kernel size to 3x3
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=7, padding=3)  # Changed kernel size to 7x7
+        self.fc1 = nn.Linear(7*7*64, 256)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 7*7*64)
+        x = F.relu(self.fc1(x))
+        return F.log_softmax(self.fc2(x), dim=1)
+
 # Load Data
 transform = transforms.Compose([
     transforms.ToTensor(),
